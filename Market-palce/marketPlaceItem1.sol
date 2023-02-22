cb8d1c3cae500add1c90bcb56eb2b4ad4386fc16
0xLeoDev <0xLeoDev@proton.me>
2023-02-22 18:45:16 +0100

    using Chainlink Price feeds

--- Market-palce/marketPlaceItem1.sol
+++ Market-palce/marketPlaceItem1.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; //import chailing rate for specific a->b
+
 
 
 contract marketPlaceItem1{
 
 mapping (address => bool) public alreadyBought;
-uint public price = 10;
+uint public price = 10 * (10**18);                // price in USD in decimals
 address public owner = payable(msg.sender);       // We automatically sending the funds to the wallet to increase the security.
+
+AggregatorV3Interface internal priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
 
 IERC20 public USDCfToken = IERC20(0xd9145CCE52D386f254917e481eB44e9943F39138);
 
-
+//-----------------------------------------------------------------//
 // we pay in token, so the function do not have to be payable
 function payInUSDCf() public returns(bool){
     require(alreadyBought[msg.sender] ==  false, "You alreay bought this item");    // Checking if "msg" already bought
@@ -22,6 +26,24 @@ function payInUSDCf() public returns(bool){
     return alreadyBought[msg.sender];
 }
 
+//-----------------------------------------------------------------//
+function getCurrentPricceOfETH() public view returns (int){
+    (/*data1*/, int priceOfUSD, /*data2*/, /*data3*/, /*data4*/) = priceFeed.latestRoundData();
+    return priceOfUSD / 10**8;
+}
+function getPriceInETH() public view  returns(int){
+    return int(price) / getCurrentPricceOfETH();
+}
+function payInETH() public payable returns(bool){
+//    require(alreadyBought[msg.sender] ==  false, "You alreay bought this item");    // Checking if "msg.sender" already bought
+
+    require(msg.value == uint(getPriceInETH()), "Wrong amount of ETH");
+    (bool sent, /*data*/) = owner.call{value: msg.value}("");
+    require(sent == true, "Failed to send ETH");
+
+    alreadyBought[msg.sender] = true;    // Adding the address to the already bought list.
+    return alreadyBought[msg.sender];
+}
 
 
 // msg.sender -> marketPlaceItem1 -> USDCfTocken
